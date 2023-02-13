c692dfd27464c9cca2a36bb62957151a77d17ca6
Patrick Wain <pat.mc7201@gmail.com>
2023-02-12 20:45:40 -0400

    initial commit

---
+++ ChessClock/main.c
@@ -0,0 +1,18 @@
+/*
+ * ChessClock.c
+ *
+ * Created: 2023-02-12 8:37:24 PM
+ * Author : patmc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
