f5b4d3e510634f0b16abfb6ff4363bb35936b5d7
Patrick Wain <patwain0603@gmail.com>
2023-02-26 13:53:58 -0400

    main header for globals, hardware init for ports

--- ChessClock/ChessClock.h
+++ ChessClock/ChessClock.h
@@ -8,7 +8,16 @@
 
 #ifndef CHESSCLOCK_H_
 #define CHESSCLOCK_H_
-
+/* global variables */
+unsigned char tick;	//for counting timer overflows
+unsigned char dig_7seg_select;	//keep track of what 7seg to output to
+char white_OR_black = 0;	//flag for whose turn it is, 0 for white, 1 for black
+unsigned int white_ms_left;		//number of ms left for white, range 0-1000,	2 bytes
+unsigned char white_s_left;		//number of s left for white, range 0-60,		1 byte
+unsigned char white_min_left;	//number of min left for white, range 0-99,		1 byte
+unsigned int black_ms_left;		//number of ms left for black, range 0-1000,	2 bytes
+unsigned char black_s_left;		//number of s left for black, range 0-60,		1 byte
+unsigned char black_min_left;	//number of min left for black, range 0-99,		1 byte
 
 
 
