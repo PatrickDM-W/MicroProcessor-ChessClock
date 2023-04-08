4adfe7eac46038f8db0a42432627ba780367d001
Patrick Wain <patwain0603@gmail.com>
2023-04-08 12:02:46 -0300

    interrupt is working, output is not

--- ChessClock/Initializations.c
+++ ChessClock/Initializations.c
@@ -17,6 +17,10 @@ void init_timer0(void)
 	GTCCR = 0x00;	//general timer/counter control reg, this is redundancy
 	TCCR0A = 0x00;	//pg 86 of atmega328p sheet no need for special modes
 	TCCR0B = 0x02;	//pg 87, prescale by 8
+	TCNT0 = 0;		//start count at 0
+	
+	TIMSK0 = 0x01;	//enable overflow interrupt
+	
 	/*
 	prescale by 8, 16MHz clock source
 	new clock source = 16M/8 = 2M
